725fd4c4687a4e78d221851ea29978b6a085e773
newbi462 <isthisemailfree@gmail.com>
2020-04-08 21:37:30 -0400

    Create a file called item.py and add an Item class in there.

---
+++ src/items.py
@@ -0,0 +1,5 @@
+class Items:
+    def __init__(self, item_name, description, location):
+        self.item_name = item_name
+        self.description = description
+        self.item_name = location
